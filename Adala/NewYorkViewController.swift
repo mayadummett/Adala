cb85e680074403ea6308af322d0b42ed2968b7ed
Maya Dummett <67915049+mayadummett@users.noreply.github.com>
2021-08-05 19:05:21 -0400

    added constraints plus directions for the assistance buttons

--- Adala/NewYorkViewController.swift
+++ Adala/NewYorkViewController.swift
@@ -14,6 +14,7 @@ class NewYorkViewController: UIViewController {
 
         // Do any additional setup after loading the view....
         // extra comment
+        //extra comment extra code testing
     }
     
   
